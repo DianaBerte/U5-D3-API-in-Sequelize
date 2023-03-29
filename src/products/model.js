2162dcb594b3f8e6a559005b5033826181aa54b9
DianaBerte <dianaberte.go@gmail.com>
2023-03-29 15:49:14 +0200

    Creates ProductsModel

---
+++ src/products/model.js
@@ -0,0 +1,45 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../db";
+
+// "id": 1,
+// "name": TEXT,
+// "category": TEXT, 
+// "description": TEXT, 
+// "imageUrl":TEXT,
+// "price":NUMBER,
+// "createdAt": "DATE",
+// "updatedAt": "DATE", 
+
+const ProductsModel = sequelize.define(
+    "product",
+    {
+        productId: {
+            type: DataTypes.UUID,
+            primaryKey: true,
+            defaultValue: DataTypes.UUIDV4,
+        },
+        "name": {
+            type: DataTypes.STRING(50),
+            allowNull: false,
+        },
+        "category":
+        {
+            type: DataTypes.STRING(50),
+            allowNull: false,
+        },
+        "description": {
+            type: DataTypes.STRING(50),
+            allowNull: false,
+        },
+        "imageURL": {
+            type: DataTypes.STRING(350),
+            allowNull: false,
+        },
+        "price": {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+    }
+)
+
+export default ProductsModel
